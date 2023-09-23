c26d4663f042148b604edfd6a1ba79076813ca8e
saikat <tawhidulislam3482@gmail.com>
2023-09-23 21:46:54 +0600

    finish

--- src/Nav-Section/Phones/PhoneCard.jsx
+++ src/Nav-Section/Phones/PhoneCard.jsx
@@ -60,7 +60,7 @@ const PhoneCard = ({ phone }) => {
                 <div className="p-6 pt-0">
                     <Link to={`/phones/${id}`} state={phone_name}>
                         <button
-                            className="block w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            className="block bg-blue-500 text-white w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                             type="button"
                         >
                             Favorite
